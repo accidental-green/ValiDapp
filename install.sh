88afc1415711f828ec590851107c47f355c422d4
Accidental Green <agstakingco@gmail.com>
2024-11-10 14:27:18 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -12,13 +12,8 @@ get_ubuntu_version() {
 install_python_and_dependencies() {
   echo "Installing dependencies for Ubuntu $1"
   sudo apt update
-  if [ "$1" == "24.04" ]; then
-    # Special handling for Ubuntu 24.04
-    sudo apt install -y python3 python3-pip python3-requests libfuse2
-  else
-    sudo apt install -y python3 python3-pip libfuse2
-    sudo pip3 install requests
-  fi
+  sudo apt install -y python3 python3-pip libfuse2
+  sudo pip3 install requests
 }
 
 # Function to install ValiDapp
@@ -27,6 +22,15 @@ install_validapp() {
   wget https://github.com/accidental-green/ValiDapp/releases/download/v1.0.0-alpha/ValiDapp-1.0.0.AppImage -O ValiDapp-1.0.0.AppImage
   chmod +x ValiDapp-1.0.0.AppImage
   sudo mv ValiDapp-1.0.0.AppImage /usr/bin/validapp
+
+  # Update permissions for chrome-sandbox to enable sandboxing
+  echo "Setting permissions for chrome-sandbox..."
+  if [[ -f "/usr/bin/validapp/chrome-sandbox" ]]; then
+    sudo chmod 4755 /usr/bin/validapp/chrome-sandbox
+  else
+    echo "Warning: chrome-sandbox not found in /usr/bin/validapp. Please ensure it is correctly bundled."
+  fi
+
   echo "ValiDapp has been successfully installed. You can run it by typing 'validapp'"
 }
 
